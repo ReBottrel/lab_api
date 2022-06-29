d2010bca9943a3eb9ca514401ecff5db380fc771
Eller Jones Silveira <ellernetpar@gmail.com>
2022-06-29 16:18:35 -0300

    .

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Exam;
+use App\Models\Animal;
 use App\Models\OrderRequest;
 use Illuminate\Http\Request;
 
@@ -27,8 +29,13 @@ class OrderController extends Controller
 
         if($id) {
             $data = $data->where('id',$id)->first();
+            if(isset($data->data_g['animal_id'])) $data->animal = Animal::with('owner', 'resenhas')->find($data->data_g['animal_id']);
         }else{
             $data = $data->paginate($request->per_page ?? 20);
+            $data->map(function($query){
+                if(isset($query->data_g['animal_id'])) $query->animal = Animal::with('owner', 'resenhas')->find($query->data_g['animal_id']);
+                return $query;
+            });
         }
         
         return response()->json($data, 200);
@@ -37,9 +44,21 @@ class OrderController extends Controller
     public function orderRequestPost(Request $request)
     {
         $user = user_token();
-        $order_request = collect($request->all())->put('origin', 'site')->put('user_id', $user->id);
+        $order_request = collect($request->all())->put('origin', 'site')->put('user_id', $user->id)->map(function($query){
+            if(isset($query['exam_id'])) $query['exam'] = collect(Exam::find($query['exam_id']))->forget(['created_at', 'updated_at'])->toArray();
+            return $query;
+        });
         $order_request = OrderRequest::create($order_request->toArray());
 
         return response()->json(OrderRequest::with('user')->find($order_request->id));
+    }
+
+    public function labOrderPut(Request $request)
+    {
+        if(isset($request->id)){
+            if(isset($request->update_status)) OrderRequest::find($request->id)->update(['status' => $request->update_status]);
+
+            return response()->json(OrderRequest::find($request->id));
+        }
     }
 }
