dab1c40faeef746164971d3d15edf3d2f0dda253
Eller Jones Silveira <ellernetpar@gmail.com>
2022-06-30 18:06:33 -0300

    .

--- app/Http/Controllers/GatewayController.php
+++ app/Http/Controllers/GatewayController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class GatewayController extends Controller
 {
-    //
+    public function generateToken()
+    {
+        $data = [
+            "email" => env('IOPLAY_EMAIL'),
+            "secret" => env('IOPLAY_SECRET'),
+            "io_seller_id" => env('IOPLAY_IO_SELLER_ID')
+        ];
+        $response = Http::post(env('IOPLAY_URL').'auth/login', $data)->object();
+        \Log::info(collect($response));
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ExamController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\AnimalController;
+use App\Http\Controllers\GatewayController;
 use App\Http\Controllers\MarkingController;
 
 /*
@@ -44,9 +45,22 @@ Route::middleware(['auth.token'])->group(function(){
     Route::get('lab/order/request/{id?}', [OrderController::class, 'orderRequestGet'])->name('orderRequestGet');
     Route::post('lab/order/request', [OrderController::class, 'orderRequestPost'])->name('orderRequestPost');
     Route::put('lab/order/request', [OrderController::class, 'labOrderPut'])->name('labOrderPut');
+
+
+    #########CONTROLE DE GATEWAY#########
+    Route::get('gateway/token', [GatewayController::class, 'generateToken'])->name('generateToken');
+    #####################################
 });
 
 // Login e Registro
 Route::middleware(['auth.token'])->get('auth/user', [AuthController::class, 'getUser'])->name('getUser');
 Route::post('auth/login', [AuthController::class, 'login'])->name('login');
 Route::post('register', [AuthController::class, 'register'])->name('register');
+
+// callbacks
+Route::get('callback/{callback}', function(\Request $request, $callback = null){
+    dd(collect([
+        $callback,
+        $request->all()
+    ]));
+});
