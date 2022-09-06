950b096dac1dacbe4ed4a4b3d86d6dc0061964bd
Felipe Cruz <felipephplow@gmail.com>
2022-09-06 15:53:25 -0300

    dwa

--- resources/views/admin/owner.blade.php
+++ resources/views/admin/owner.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin')
 @section('content')
     <div class="container">
-        <div class="card">
+        <div class="card my-4">
             <div class="card-body">
                 <div>
                     <h4>Buscar proprietario</h4>
@@ -17,6 +17,86 @@
                     <div class="col-md-4">
                         <button class="btn btn-primary">Continuar</button>
 
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card my-4">
+            <div class="card-body">
+                <div>
+                    <h4>Cadastrar Proprietario</h4>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Nome do proprietario</label>
+                            <input type="text" class="form-control" value="{{ $order->creator }}">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">CPF/CNPJ</label>
+                            <input type="text" class="form-control">
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">CEP</label>
+                            <input type="text" class="form-control">
+                        </div>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Endereço</label>
+                            <input type="text" class="form-control">
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Numero</label>
+                            <input type="text" class="form-control">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Complemento</label>
+                            <input type="text" class="form-control">
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">UF</label>
+                            <input type="text" class="form-control">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Bairro</label>
+                            <input type="text" class="form-control">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Cidade</label>
+                            <input type="text" class="form-control">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Telefone</label>
+                            <input type="text" class="form-control">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Email</label>
+                            <input type="email" class="form-control">
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="mb-3 text-center">
+                            <button class="btn btn-success">Salvar</button>
+                        </div>
                     </div>
                 </div>
             </div>
