ddd151de36a111319cba1905a02de97cfefd9988
Felipe Cruz <fecruz2022@gmail.com>
2022-11-03 09:55:14 -0300

    mudei buttons

--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -35,25 +35,38 @@
                                     <td>{{ $owner->owner_name }}</td>
                                     <td>{{ strtolower($owner->email) }}</td>
                                     <td>
-                                        <a href="{{ route('owner.edit', $owner->id) }}"
-                                            class="btn btn-sm btn-primary">Editar</a>
-                                        <a href="{{ route('get.owners.details', $owner->id) }}"
-                                            class="btn btn-sm btn-success">Detalhes</a>
-                                        @if ($owner->user_id)
-                                            <a href="{{ route('owner.user', $owner->user_id) }}"><button type="button"
-                                                    class="btn btn-sm btn-alt-loci text-white">Ver Usuário</button></a>
-                                        @else
-                                            <button type="button" data-id="{{ $owner->id }}"
-                                                class="btn btn-sm btn-info create-access">Criar Acesso</button>
-                                        @endif
+                                        <div class="dropdown">
+                                            <a class="btn btn-alt-loci text-white dropdown-toggle" href="#"
+                                                role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                                Ações
+                                            </a>
 
-                                        @if ($owner->old_id)
-                                            <a href="{{ route('get.animals', $owner->old_id) }}"><button type="button"
-                                                    class="btn btn-sm btn-warning my-2">Ver animais</button></a>
-                                        @else
-                                            <a href="{{ route('get.animals', $owner->id) }}"><button type="button"
-                                                    class="btn btn-sm btn-warning my-2">Ver animais</button></a>
-                                        @endif
+                                            <ul class="dropdown-menu">
+                                                <a href="{{ route('owner.edit', $owner->id) }}"
+                                                    class="dropdown-item">Editar</a>
+                                                <a href="{{ route('get.owners.details', $owner->id) }}"
+                                                    class="dropdown-item">Detalhes</a>
+                                                @if ($owner->user_id)
+                                                    <a href="{{ route('owner.user', $owner->user_id) }}"><button
+                                                            type="button" class="dropdown-item">Ver
+                                                            Usuário</button></a>
+                                                @else
+                                                    <button type="button" data-id="{{ $owner->id }}"
+                                                        class="dropdown-item create-access">Criar Acesso</button>
+                                                @endif
+
+                                                @if ($owner->old_id)
+                                                    <a href="{{ route('get.animals', $owner->old_id) }}"><button
+                                                            type="button" class="dropdown-item">Ver
+                                                            animais</button></a>
+                                                @else
+                                                    <a class="dropdown-item" href="{{ route('get.animals', $owner->id) }}">
+                                                        >Ver animais</a>
+                                                @endif
+                                                <a href="#" class="dropdown-item">Propriedades</a>
+                                            </ul>
+                                        </div>
+
                                     </td>
                                 </tr>
                             @endforeach
