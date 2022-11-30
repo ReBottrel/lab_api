41f537162ad8c83e9e8a37e277399de1d88db229
Felipe Cruz <fecruz2022@gmail.com>
2022-11-30 14:26:27 -0300

    123124

--- resources/views/admin/order-add-animal.blade.php
+++ resources/views/admin/order-add-animal.blade.php
@@ -79,9 +79,10 @@
                                     <input type="text" name="animal_name" id="animal_name" class="form-control">
                                 </div>
                             </div>
+                  
                             <div class="col-md-6">
                                 <div class="mb-3">
-                                    <label for="exampleFormControlInput1" class="form-label">Numero de registro</label>
+                                    <label for="exampleFormControlInput1" class="form-label">ID do animal</label>
                                     <input type="text" name="register_number_brand" id="register_number_brand"
                                         class="form-control">
                                 </div>
