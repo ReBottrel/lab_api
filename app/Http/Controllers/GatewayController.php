e1d5aa5149bdab90343231136c141033aed9b6c2
Felipe Cruz <fecruz2022@gmail.com>
2022-11-30 16:32:51 -0300

    321

--- app/Http/Controllers/GatewayController.php
+++ app/Http/Controllers/GatewayController.php
@@ -302,7 +302,7 @@ class GatewayController extends Controller
             ];
 
             $response = Http::withHeaders(['Authorization' => $this->bearer_token])->post(env('IOPAY_URL') . 'v1/customer/new', $data)->object();
-            \Log::info($response);
+            \Log::info(json_encode($response));
             UserInfo::where('user_id', $user->id)->update(['buyer_id' => ($response->success->id ?? $response->id)]);
             return ($response->success->id ?? $response->id);
         }
