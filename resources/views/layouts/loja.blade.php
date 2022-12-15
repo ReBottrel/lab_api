d3c59ef966f471544b539a1eec4574ce36f1e83f
Felipe Cruz <fecruz2022@gmail.com>
2022-12-15 11:18:02 -0300

    1231

--- resources/views/layouts/loja.blade.php
+++ resources/views/layouts/loja.blade.php
@@ -4,7 +4,11 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('loja/node_modules/bootstrap-icons/font/bootstrap-icons.css') }}">
     <script src="https://kit.fontawesome.com/0ab2bcde1c.js" crossorigin="anonymous"></script>
@@ -128,7 +132,8 @@
                     <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
                         <!-- Links -->
                         <h6 class="text-uppercase fw-bold mb-4">Contato</h6>
-                        <p><i class="fas fa-home me-3 text-primary"></i>Rua Coronel Durães, 170, slj 01, Bela Vista - Lagoa Santa l MG</p>
+                        <p><i class="fas fa-home me-3 text-primary"></i>Rua Coronel Durães, 170, slj 01, Bela Vista -
+                            Lagoa Santa l MG</p>
                         <p>
                             <i class="fas fa-envelope me-3 text-primary"></i>
                             atendimento@locilab.com.br
--- resources/views/loja/home.blade.php
+++ resources/views/loja/home.blade.php
@@ -2,7 +2,9 @@
 @section('content')
     <div class="container">
         <div class="d-flex">
-            <h4>EM BREVE E-COMERCE COMPLETO </h4>
+            <div class="breve">
+                <h4>EM BREVE E-COMERCE COMPLETO </h4>
+            </div>
         </div>
     </div>
 @endsection
