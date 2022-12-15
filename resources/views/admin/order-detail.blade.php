e4c44076d5de4810292dd47f992886392d67d567
Felipe Cruz <fecruz2022@gmail.com>
2022-12-15 17:31:02 -0300

    4124

--- resources/views/admin/order-detail.blade.php
+++ resources/views/admin/order-detail.blade.php
@@ -389,7 +389,7 @@
                         },
                         success: function(data) {
                             console.log(data);
-                            window.location.reload();
+                            // window.location.reload();
                         }
                     }));
                 if (result.isConfirmed) {
