7f189ba1dc103a03a02d780c45762c250300de5a
Felipe Cruz <fecruz2022@gmail.com>
2023-01-31 21:36:08 -0300

    4124

---
+++ resources/views/loja/privacy-policies.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.loja')
+
+@section('content')
+    <div class="container">
+        <div class="card">
+            <div class="card-body">
+                <div>
+                    <p>Finalidade: O LOCI LAB coleta e armazena informações pessoais de seus clientes para realizar testes
+                        de genética animal e fornecer resultados precisos e confiáveis. Também usamos informações de
+                        pagamento para processar transações financeiras.
+                    </p>
+                    </p> Tipos de informações coletadas: Coletamos informações pessoais, incluindo nome, endereço, endereço
+                    de e-mail, número de telefone, informações sobre seu animal de estimação e informações de pagamento,
+                    como número de cartão de crédito ou débito.<p>
+                    <p>
+                        Compartilhamento de informações: Compartilhamos informações de pagamento com a IOPAY, uma empresa de
+                        processamento de pagamentos, para processar transações financeiras. Não compartilhamos nenhuma outra
+                        informação pessoal com terceiros, exceto quando exigido por lei.
+                    </p>
+                    <p>
+                        Armazenamento de informações: Armazenamos suas informações pessoais de forma segura e confidencial
+                        por um período de tempo razoável, de acordo com as leis aplicáveis e nossos próprios padrões de
+                        privacidade.
+                    </p>
+                    <p> Direitos do usuário: Você tem o direito de acessar, corrigir e excluir suas informações pessoais a
+                        qualquer momento. Você também pode optar por não fornecer informações pessoais, mas isso pode afetar
+                        nossa capacidade de fornecer serviços completos.</p>
+                    <p>
+                        Alterações no termo de privacidade: Reservamos o direito de fazer alterações em nosso termo de
+                        privacidade a qualquer momento. Qualquer alteração será publicada em nosso site e entrará em vigor
+                        imediatamente após a publicação.
+                    </p>
+
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
