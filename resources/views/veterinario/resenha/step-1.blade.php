47db7a2ed06e5196058ea7bca4314274aae00d5e
Felipe Cruz <fecruz2022@gmail.com>
2023-02-13 10:10:45 -0300

    42141

--- resources/views/veterinario/resenha/step-1.blade.php
+++ resources/views/veterinario/resenha/step-1.blade.php
@@ -15,7 +15,10 @@
                 <button class="btn btn-primary" onclick="draw()">Desenhar</button>
             </div>
             <div>
-                <button class="btn btn-success" onclick="cancelDraw()">sair do desenho</button>
+                <button class="confirm-draw" onclick="cancelDraw()"><i class="fa-solid fa-circle-check"></i></button>
+            </div>
+            <div>
+                <button class="undo-draw" onclick="undo()"><i class="fa-solid fa-eraser"></i></button>
             </div>
             <div>
                 <button class="btn btn-success" id="salvar">SALVAR</button>
@@ -30,7 +33,48 @@
         canvas.selection = false;
         canvas.setWidth(window.innerWidth);
         canvas.setHeight(600);
+        canvas.on('object:added', function() {
+            if (!isRedoing) {
+                h = [];
+            }
+            isRedoing = false;
+        });
 
+        var isRedoing = false;
+        var h = [];
+
+        var deleteIcon =
+            "data:image/svg+xml,%3C%3Fxml version='1.0' encoding='utf-8'%3F%3E%3C!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'%3E%3Csvg version='1.1' id='Ebene_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='595.275px' height='595.275px' viewBox='200 215 230 470' xml:space='preserve'%3E%3Ccircle style='fill:%23F44336;' cx='299.76' cy='439.067' r='218.516'/%3E%3Cg%3E%3Crect x='267.162' y='307.978' transform='matrix(0.7071 -0.7071 0.7071 0.7071 -222.6202 340.6915)' style='fill:white;' width='65.545' height='262.18'/%3E%3Crect x='266.988' y='308.153' transform='matrix(0.7071 0.7071 -0.7071 0.7071 398.3889 -83.3116)' style='fill:white;' width='65.544' height='262.179'/%3E%3C/g%3E%3C/svg%3E";
+        var img = document.createElement('img');
+        img.src = deleteIcon;
+
+        fabric.Object.prototype.controls.deleteControl = new fabric.Control({
+            x: 0.5,
+            y: -0.5,
+            offsetY: 16,
+            cursorStyle: 'pointer',
+            mouseUpHandler: deleteObject,
+            render: renderIcon,
+            cornerSize: 24
+        });
+
+        // Add();
+
+        function deleteObject(eventData, transform) {
+            var target = transform.target;
+            var canvas = target.canvas;
+            canvas.remove(target);
+            canvas.requestRenderAll();
+        }
+
+        function renderIcon(ctx, left, top, styleOverride, fabricObject) {
+            var size = this.cornerSize;
+            ctx.save();
+            ctx.translate(left, top);
+            ctx.rotate(fabric.util.degreesToRadians(fabricObject.angle));
+            ctx.drawImage(img, -size / 2, -size / 2, size, size);
+            ctx.restore();
+        }
 
         canvas.setBackgroundImage('{{ asset('vet/img/step-1.png') }}', function() {
             let img = canvas.backgroundImage;
@@ -54,6 +98,7 @@
             canvas.isDrawingMode = true;
             canvas.freeDrawingBrush.width = 5;
             canvas.freeDrawingBrush.color = '#000';
+
             canvas.on('path:created', function(e) {
 
                 e.path.set();
@@ -61,13 +106,22 @@
 
 
             });
+            $('.confirm-draw').show();
+            $('.undo-draw').show();
         }
 
         function cancelDraw() {
             canvas.isDrawingMode = false;
+            $('.confirm-draw').hide();
+            $('.undo-draw').hide();
         }
 
-
+        function undo() {
+            if (canvas._objects.length > 0) {
+                h.push(canvas._objects.pop());
+                canvas.renderAll();
+            }
+        }
 
 
         function addImage() {
