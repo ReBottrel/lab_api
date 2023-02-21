b137ee4c26245985eba24a5ba1885ab8d1275897
Felipe Cruz <fecruz2022@gmail.com>
2023-02-21 10:17:53 -0300

    awe

--- app/Http/Controllers/Admin/ApiMangalargaController.php
+++ app/Http/Controllers/Admin/ApiMangalargaController.php
@@ -17,7 +17,7 @@ class ApiMangalargaController extends Controller
 {
     public function __construct()
     {
-        ini_set('max_execution_time', 700);
+        ini_set('max_execution_time', 5000);
     }
     public function getApi()
     {
