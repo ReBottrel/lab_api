2feb9dc8740a79c7a52cef52e2bb17caff266468
Felipe Cruz <fecruz2022@gmail.com>
2023-03-06 14:53:25 -0300

    dawfa

--- resources/views/veterinario/owner.blade.php
+++ resources/views/veterinario/owner.blade.php
@@ -30,26 +30,29 @@
                                 </div>
                                 <div class="mb-3 cad-animal-content-input">
                                     <label for="exampleFormControlInput1" class="form-label">Email</label>
-                                    <input type="text" class="form-control" name="register_number_brand">
+                                    <input type="text" class="form-control" name="email">
                                 </div>
                             </div>
-                            <div class="my-3 text-end">
-                                <button type="button" class="btnNext btn btn-alt-1">Próximo</button>
+                            <div class="buttons">
+                                <div class="my-3 text-end">
+                                    <button type="button" class="btnNext btn btn-alt-1">Próximo</button>
+                                </div>
                             </div>
                         </fieldset>
                         <fieldset class="hidden">
                             <div class="cad-animal-content">
                                 <div class="cad-animal-content-title">
-                                    <h2>Informações do animal</h2>
+                                    <h5>Contatos do proprietário</h5>
                                 </div>
                                 <div class="mb-3 cad-animal-content-input">
-                                    <label for="exampleFormControlInput1" class="form-label">Nome do animal</label>
+                                    <label for="exampleFormControlInput1" class="form-label">Telefone</label>
                                     <input type="text" class="form-control" name="animal_name">
                                 </div>
                                 <div class="mb-3 cad-animal-content-input">
-                                    <label for="exampleFormControlInput1" class="form-label">Numero do registro</label>
+                                    <label for="exampleFormControlInput1" class="form-label">Whatsapp</label>
                                     <input type="text" class="form-control" name="register_number_brand">
                                 </div>
+
                             </div>
                             <div class="buttons">
                                 <div class="my-3 text-end">
@@ -59,8 +62,50 @@
                                     <button type="button" class="btnNext btn btn-alt-1">Próximo</button>
                                 </div>
                             </div>
+                        </fieldset>
+                        <fieldset class="hidden">
+                            <div class="cad-animal-content">
+                                <div class="cad-animal-content-title">
+                                    <h5>Endereço do proprietário</h5>
+                                </div>
+                                <div class="mb-3 cad-animal-content-input">
+                                    <label for="zip_code" class="form-label">CEP</label>
+                                    <input type="text" class="form-control" name="zip_code" id="zip_code">
+                                </div>
 
+                                <div class="mb-3 cad-animal-content-input">
+                                    <label for="address" class="form-label">Endereço</label>
+                                    <input type="text" class="form-control" name="address" id="address">
+                                </div>
 
+                                <div class="mb-3 cad-animal-content-input">
+                                    <label for="number" class="form-label">Número</label>
+                                    <input type="text" class="form-control" name="number" id="number">
+                                </div>
+
+                                <div class="mb-3 cad-animal-content-input">
+                                    <label for="complement" class="form-label">Complemento</label>
+                                    <input type="text" class="form-control" name="complement" id="complement">
+                                </div>
+
+                                <div class="mb-3 cad-animal-content-input">
+                                    <label for="district" class="form-label">Bairro</label>
+                                    <input type="text" class="form-control" name="district" id="district">
+                                </div>
+                                <div class="mb-3 cad-animal-content-input">
+                                    <label for="city" class="form-label">Cidade</label>
+                                    <input type="text" class="form-control" name="city" id="city">
+                                </div>
+
+                            </div>
+                            <div class="buttons">
+                                <div class="my-3 text-end">
+                                    <button type="button" class="btnPrev btn btn-alt-1">Anterior</button>
+                                </div>
+                                <div class="my-3 text-end">
+                                    <button type="button" class="btnNext btn btn-alt-1">Próximo</button>
+                                </div>
+                            </div>
                         </fieldset>
                     </form>
                 </div>
@@ -78,16 +123,40 @@
             $('.btn-create').click(function() {
                 window.location.href = "{{ route('animal.create') }}";
             });
+
             $('.btnNext').click(function() {
-                $('fieldset').slideToggle('slow', function() {
-                    $(this).toggleClass('hidden');
+                var $this = $(this);
+                var $current = $this.parents('fieldset');
+                var $next = $current.next('fieldset');
+
+                $current.animate({
+                    opacity: 0
+                }, 500, function() {
+                    $current.addClass('hidden');
+                    $next.removeClass('hidden');
+                    $next.animate({
+                        opacity: 1
+                    }, 500);
                 });
             });
+
             $('.btnPrev').click(function() {
-                $('fieldset').slideToggle('slow', function() {
-                    $(this).toggleClass('hidden');
+                var $this = $(this);
+                var $current = $this.parents('fieldset');
+                var $prev = $current.prev('fieldset');
+
+                $current.animate({
+                    opacity: 0
+                }, 500, function() {
+                    $current.addClass('hidden');
+                    $prev.removeClass('hidden');
+                    $prev.animate({
+                        opacity: 1
+                    }, 500);
                 });
             });
+         
+
         });
     </script>
 @endsection
