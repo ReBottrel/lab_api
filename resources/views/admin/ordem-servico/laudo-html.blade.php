fafec00e887c033d6a8fe8d59dc1d53c6da538da
Simetria Studio <github@simetria.studio>
2023-06-09 21:36:47 -0300

    suba

--- resources/views/admin/ordem-servico/laudo-html.blade.php
+++ resources/views/admin/ordem-servico/laudo-html.blade.php
@@ -1,24 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.admin')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laudo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('adm/assets/laudo/css/laudo-html.min.css') }}">
-</head>
+@section('title', 'Laudo')
 
-<body>
+@section('content')
     <div class="container">
         <div class="my-2">
             <h1 class="text-center fs-3"><strong>Laudo Veterinário</strong></h1>
         </div>
         <div class="card mb-3">
             <div class="informacoes">
-                <div class="text-center">
-                    <h1>Dados Relativos à Amostra</h1>
+                <div class="text-center text-decoration-underline my-3">
+                    <h1 class="fs-4">Dados Relativos à Amostra</h1>
                 </div>
                 <div class="row">
                     <div class="col-6">
@@ -71,53 +63,156 @@
                         <p><strong>Observações:</strong> [Observações]</p>
                     </div>
                 </div>
+                <div class="text-center text-decoration-underline my-3">
+                    <h1 class="fs-4">Dados Relativos ao Ensaio</h1>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <p><strong>Data da Realização:</strong> [dd/mm/yyyy]</p>
+                    </div>
+                    <div class="col-12">
+                        <p><strong>Metodologia Utilizada:</strong> Identificação Genética e Pesquisa de Vínculo Genético
+                            pela amplificação das regiões STRs pela técnica PCR e
+                            detecção por eletroforese capilar em sistema automatizado por fluorescência laser-induzida
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="card mb-3">
-            <h2>Diagnóstico</h2>
-            <p>[Descrição detalhada do diagnóstico do equino]</p>
-
-            <h2>Tratamento Recomendado</h2>
-            <p>[Descrição detalhada do tratamento recomendado para o equino]</p>
-
-            <h2>Recomendações</h2>
-            <ul>
-                <li>[Recomendação 1]</li>
-                <li>[Recomendação 2]</li>
-                <li>[Recomendação 3]</li>
-                <!-- Adicione mais recomendações, se necessário -->
-            </ul>
-
-            <h2>Assinatura do Veterinário</h2>
-            <p>[Nome do veterinário]</p>
-            <p>[Cargo do veterinário]</p>
-
-
-
-            <h1>Laudo de DNA</h1>
-
-            <h2>Amostras</h2>
-            <p><strong>Amostra 1:</strong> [Descrição da amostra 1]</p>
-            <p><strong>Amostra 2:</strong> [Descrição da amostra 2]</p>
-
-            <h2>Resultados</h2>
-            <p>[Descrição dos resultados obtidos a partir da análise de DNA]</p>
-
-            <h2>Análise</h2>
-            <p>[Análise e interpretação dos resultados]</p>
-
-            <h2>Conclusão</h2>
-            <p>[Conclusão do laudo de DNA]</p>
-
-            <h2>Assinatura do Perito</h2>
-            <p>[Nome do perito]</p>
-            <p>[Cargo do perito]</p>
+            <div class="text-center my-3 text-decoration-underline">
+                <h1 class="fs-4">Tabela de Resultados</h1>
+            </div>
+            <div class="table-responsive">
+                <table class="table table-bordered border-dark table-sm">
+                    <thead>
+                        <tr>
+                            <th scope="col">Nome</th>
+                            <th scope="col">OLINDA DA GROTA VIVA</th>
+                            <th scope="col">SONHADOR REAL DE MAUÁ</th>
+                            <th scope="col">IMPACTO DO LUAL</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">N Relatório de Ensaio</th>
+                            <th scope="row">ECVP13-14209</th>
+                            <th scope="row">LO22-68946</th>
+                            <th scope="row">01516330.ECVP17-26251</th>
+                        </tr>
+                        <tr>
+                            <th scope="row">Microssatélites</th>
+                            <th scope="row">Alelos</th>
+                            <th scope="row">Alelos</th>
+                            <th scope="row">Alelos</th>
+                        </tr>
+                        <tr>
+                            <td>AHT4</td>
+                            <td>H - M</td>
+                            <td>K - M</td>
+                            <td>K - N</td>
+                        </tr>
+                        <tr>
+                            <td>AHT5</td>
+                            <td>J - N</td>
+                            <td>J - N</td>
+                            <td>J - J</td>
+                        </tr>
+                        <tr>
+                            <td>ASB2</td>
+                            <td>C - I</td>
+                            <td>I - M</td>
+                            <td>* - *</td>
+                        </tr>
+                        <tr>
+                            <td>ASB23</td>
+                            <td>J - K</td>
+                            <td>J - K</td>
+                            <td>J - J</td>
+                        </tr>
+                        <tr>
+                            <td>HMS2</td>
+                            <td>H - I</td>
+                            <td>I - K</td>
+                            <td>K - L</td>
+                        </tr>
+                        <tr>
+                            <td>HMS3</td>
+                            <td>O - P</td>
+                            <td>P - P</td>
+                            <td>P - P</td>
+                        </tr>
+                        <tr>
+                            <td>HMS6</td>
+                            <td>O - P</td>
+                            <td>O - O</td>
+                            <td>O - P</td>
+                        </tr>
+                        <tr>
+                            <td>HMS7</td>
+                            <td>K - L</td>
+                            <td>L - N</td>
+                            <td>K - N</td>
+                        </tr>
+                        <tr>
+                            <td>HTG10</td>
+                            <td>* - *</td>
+                            <td>M - O</td>
+                            <td>* - *</td>
+                        </tr>
+                        <tr>
+                            <td>HTG4</td>
+                            <td>L - L</td>
+                            <td>L - M</td>
+                            <td>L - L</td>
+                        </tr>
+                        <tr>
+                            <td>HTG7</td>
+                            <td>O - O</td>
+                            <td>O - O</td>
+                            <td>O - O</td>
+                        </tr>
+                        <tr>
+                            <td>VHL20</td>
+                            <td>L - M</td>
+                            <td>L - M</td>
+                            <td>L - M</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
 
-    </div>
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-
-</html>
+        <div class="card mb-3">
+            <div class="text-center my-3 text-decoration-underline">
+                <h1 class="fs-4">Resultados</h1>
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-12">
+                        <p>
+                            <strong>Conclusão:</strong> Conclui-se que o produto SONHADO REAL DE MAUÁ está qualificado pela
+                            genitora OLINDA DA GROTA VIVA (149467) e está qualificado
+                            pelo genitor IMPACTO DO LUAL (53753).
+                        </p>
+                    </div>
+                    <div class="col-12">
+                        <p>
+                            <strong>Observações:</strong> O Resultado da anàlise de vinculo apresentado aqui foi definido
+                            com base nos seguintes laudos:
+                        </p>
+                        <p>
+                            GENITORA: animal OLINDA DA GROTA VIVA, número ECVP13-14209, emitido pelo laboratório Linhagen
+                            em
+                            16/05/2017.
+                            FILHO(A): animal SONHADO REAL DE MAUÁ, número LO22-68946, emitido pelo laboratório Loci Genética
+                            Laboratorial em 05/05/2022.
+                            GENITOR: animal IMPACTO DO LUAL, número 01516330.ECVP17-26251, emitido pelo laboratório Linhagen
+                            em 30/07/2021.
+                            Esses laudos são de exclusiva responsabilidade dos laboratórios emissores.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endsection
