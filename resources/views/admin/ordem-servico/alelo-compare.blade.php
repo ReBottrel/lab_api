d4e082c323dd274ad51c90b75b173428260b4c47
Felipe Cruz <fecruz2022@gmail.com>
2023-06-29 13:46:55 -0300

    as

--- resources/views/admin/ordem-servico/alelo-compare.blade.php
+++ resources/views/admin/ordem-servico/alelo-compare.blade.php
@@ -11,13 +11,13 @@
             @endphp
 
             <div class="col-2 bg-light border rounded text-center">
-                <h5>{{ $mae->id ?? 'Sem verificação' }}</h5>
+                <h5>{{ $mae->codlab ?? 'Sem verificação' }}</h5>
             </div>
             <div class="col-3 bg-light border rounded text-center">
-                <h5>{{ $animal->id ?? 'Nao encontrado' }}</h5>
+                <h5>{{ $animal->codlab ?? 'Nao encontrado' }}</h5>
             </div>
             <div class="col-2 bg-light border rounded text-center">
-                <h5>{{ $pai->id ?? 'Sem verificação' }}</h5>
+                <h5>{{ $pai->codlab ?? 'Sem verificação' }}</h5>
             </div>
             <div class="col-3 bg-light border rounded text-center">
                 <button type="button" data-ordem="{{ $ordem->id }}" id="analisar"
