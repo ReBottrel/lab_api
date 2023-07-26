bbca32b8c05b96e41129d29fd01b89482afe6c5a
Felipe Cruz <fecruz2022@gmail.com>
2023-07-26 09:07:57 -0300

    fawf

--- resources/views/admin/ordem-servico/resultado-busca.blade.php
+++ resources/views/admin/ordem-servico/resultado-busca.blade.php
@@ -158,7 +158,7 @@
     </div>
     <!-- Modal -->
     <div class="modal fade" id="modalData" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
+        <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
                     <h1 class="modal-title fs-5" id="exampleModalLabel">Inserir data da análise</h1>
