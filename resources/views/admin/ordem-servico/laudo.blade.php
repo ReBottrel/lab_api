dd99da2a68a58761ca31e341caa45d1b47f44c62
Felipe Cruz <fecruz2022@gmail.com>
2023-07-27 17:56:28 -0300

    fawf

--- resources/views/admin/ordem-servico/laudo.blade.php
+++ resources/views/admin/ordem-servico/laudo.blade.php
@@ -586,7 +586,7 @@
             <div class="content_3">
                 <p>
                     <strong>Responsável pela Coleta/Registro Profissional ou CPF:</strong>
-                    <span>{{ $order->tecnico ?? 'Não informado' }} - {{ $tecnico->document ?? 'Não informado' }}</span>
+                    <span>{{ $ordem->tecnico ?? 'Não informado' }} - {{ $tecnico->document ?? 'Não informado' }}</span>
                     <br>
                     <strong>Data do Recebimento</strong>
                     <span>{{ $datas->data_recebimento }}</span>
