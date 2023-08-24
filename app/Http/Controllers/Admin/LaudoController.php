7cc4abcde743e5400ddaace5748c72c7a7e0d3e0
Felipe Cruz <fecruz2022@gmail.com>
2023-08-24 11:42:16 -0300

    faw

--- app/Http/Controllers/Admin/LaudoController.php
+++ app/Http/Controllers/Admin/LaudoController.php
@@ -133,7 +133,7 @@ class LaudoController extends Controller
         $dna_verify = DnaVerify::where('animal_id', $animal->id)->latest('created_at')->first();
         $sigla = substr($animal->especies, 0, 3);
         $examType = substr($dna_verify->verify_code, 3, 2);
-        $ordem = OrdemServico::where('animal_id', $laudo->animal_id)->latest()->first();
+        $ordem = OrdemServico::where('order', $laudo->order_id)->latest()->first();
         $pai = null;
         $mae = null;
         $relation = AnimalToParent::where('animal_id', $animal->id)->first();
