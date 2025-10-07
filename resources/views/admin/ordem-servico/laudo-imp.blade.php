1b421770d46486d4e12a40a05a1f085cd9e7b326
Felipe Cruz <fecruz2022@gmail.com>
2025-10-07 14:34:01 -0300

    a

--- resources/views/admin/ordem-servico/laudo-imp.blade.php
+++ resources/views/admin/ordem-servico/laudo-imp.blade.php
@@ -881,7 +881,11 @@
                                     {{ date('d/m/Y', strtotime($laudo->data_retificacao)) }}
                                 @else
                                     @if ($animal->alelos[0]->lab == 'LOCI GENÉTICA LABORATORIAL' || $animal->alelos[0]->lab == 'Loci Genética Laboratorial')
-                                        {{ date('d/m/Y') }}
+                                        @if ($animal->id == 91528)
+                                            {{ date('d/m/Y', strtotime($animal->alelos[0]->updated_at)) }}
+                                        @else
+                                            {{ date('d/m/Y') }}
+                                        @endif
                                     @else
                                         {{ date('d/m/Y', strtotime($animal->alelos[0]->data)) }}
                                     @endif
